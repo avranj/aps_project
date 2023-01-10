0b0222dd95603a2c456ce28ef5eeb6ff88bb69e9
avranj <avranj.k@gmail.com>
2023-01-10 23:44:55 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages,setup
 
 from typing import List
+HYPHEN_E_DOT = "-e ."
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements()-> List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list=requirement_file.readlines()
     requirement_list=[requirement_name.replace("\n","")for requirement_name in requirement_list]
-    
+
     if HYPEN_E_DOT in requirement_list:
         requirement_list.remove(HYPEN_E_DOT)
     return  requirement_list     
